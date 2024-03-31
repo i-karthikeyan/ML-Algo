eae6d9eaf61051988ac5126476bdd490efff368f
Karthikeyan.S <144373234+i-karthikeyan@users.noreply.github.com>
2024-03-31 13:09:09 +0530

    Add files via upload

---
+++ NaiveBayes.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+class NaiveBayes:
+    def fit(self, X, y):
+        n_sample, n_feature = X.shape
+        self._classes = np.unique(y)
+        n_classes = len(self._classes)
+        # calculate mean,var,prior
+        self._mean = np.zeros((n_classes, n_feature),dtype=np.float64)
+        self._var = np.zeros((n_classes, n_feature),dtype=np.float64)
+        self._priors = np.zeros(n_classes,np.float64)
+        for idx, c in enumerate(self._classes):
+            X_c = X[y == c]
+            self._mean[idx, :] = X_c.mean(axis=0)
+            self._var[idx, :] = X_c.var(axis=0)
+            self._priors[idx] = X_c.shape[0] / float(n_sample)
+    def predict(self,X):
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
+    def _predict(self,x):
+        posteriors = []
+        for idx , c in enumerate(self._classes):
+            prior = np.log(self._priors[idx])
+            posterior = np.sum(np.log(self._pdf(idx,x)))
+            posterior+=prior
+            posteriors.append(posterior)
+        return self._classes[np.argmax(posteriors)]
+    def _pdf(self,class_idx,x):
+        mean = self._mean[class_idx]
+        var = self._var[class_idx]
+        numberator = np.exp(-((x-mean)**2)/(2*var))
+        denominator = np.sqrt(2*np.pi*var)
+        return numberator/denominator
