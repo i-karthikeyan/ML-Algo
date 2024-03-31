b2d6dcfaf3c42ea2967ce1b1c682f0ad59811593
Karthikeyan.S <144373234+i-karthikeyan@users.noreply.github.com>
2024-03-31 13:37:38 +0530

    Add files via upload

--- LinearReg.py
+++ LinearReg.py
@@ -1,1 +1,23 @@
 import numpy as np
+class LinearRegression:
+    def __init__(self,lr=0.001,n_iters = 1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weight = None
+        self.bias = None
+    def fit(self,X,y):
+        n_sample,n_features = X.shape
+        self.weight = np.zeros(n_features)
+        self.bias=0
+        for _ in range(self.n_iters):
+            y_pred = np.dot(X,self.weight)+self.bias
+            dw = (1/n_sample)*np.dot(X.T,(y_pred-y))
+            db = (1/n_sample)*np.sum(y_pred-y)
+            self.weight-= self.lr*dw
+            self.bias -= self.lr*db
+    def predict(self,X):
+        y_pred = np.dot(X,self.weight)+self.bias
+        return y_pred
+
+    def mse(self,y_test, predictions):
+        return np.mean((y_test - predictions) ** 2)
--- svmHelper.py
+++ svmHelper.py
@@ -1,3 +1,5 @@
+# if __name__ == "__main__":
+    # Imports
 import numpy as np
 from svm import SVM
 
